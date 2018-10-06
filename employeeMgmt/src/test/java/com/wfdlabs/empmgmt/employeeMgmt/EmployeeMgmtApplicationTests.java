b156f8c728f814632460254196e1ba77ce4b720e
bayareddy <bayareddy27@gmail.com>
2018-10-06 19:40:29 +0530

    emp mgmt

---
+++ employeeMgmt/src/test/java/com/wfdlabs/empmgmt/employeeMgmt/EmployeeMgmtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wfdlabs.empmgmt.employeeMgmt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeMgmtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
